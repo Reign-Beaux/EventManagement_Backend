fe8c51331df0f210abcfef7e97fbe56fd62f42f1
Reign-Beaux <samc920130@hotmail.com>
2024-10-07 01:01:25 -0600

    Se limpia el UserMappings por el momento

--- Application/Mappings/UserMappings.cs
+++ Application/Mappings/UserMappings.cs
@@ -1,14 +1,9 @@
-﻿using Application.UseCases.Users.Commands.InsertUser;
-using AutoMapper;
-using Domain.Entities.EventManagement.Users.Repository.Parameters;
-
-namespace Application.Mappings
+﻿namespace Application.Mappings
 {
     public class UserMappings : Profile
     {
         public UserMappings()
         {
-            CreateMap<InsertUserCommand, UserInsertParameters>();
         }
     }
 }
