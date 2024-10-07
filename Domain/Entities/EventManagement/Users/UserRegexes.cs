e6999d5f9241266589bf7e56fe7c0fc8e88d17ef
Reign-Beaux <samc920130@hotmail.com>
2024-10-07 01:01:42 -0600

    Se crea UserRegexes

---
+++ Domain/Entities/EventManagement/Users/UserRegexes.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Entities.EventManagement.Users
+{
+    public static partial class UserRegexes
+    {
+        public static readonly Regex Email = EmailRegex();
+        public static readonly Regex Password = PasswordRegex();
+        public static readonly Regex Username = UsernameRegex();
+
+        [GeneratedRegex(@"^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$")]
+        private static partial Regex EmailRegex();
+
+        [GeneratedRegex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[$%&@#])[A-Za-z\d$%&@#]{8,36}$")]
+        private static partial Regex PasswordRegex();
+
+        [GeneratedRegex(@"^[A-Za-z\d._-]{4,32}$")]
+        private static partial Regex UsernameRegex();
+    }
+}
